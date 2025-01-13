90b435bcc648d94614dc47b2f670e2fcfbeecb9b
NahomKibreab <nknahom@gmail.com>
2025-01-13 12:45:19 -0700

    Code cleanup for json-sot-string

--- src/file-converter/strategies/json-to-string.converter.ts
+++ src/file-converter/strategies/json-to-string.converter.ts
@@ -8,19 +8,37 @@ export class JsonToStringConverter implements ConversionStrategy {
     const { content, segmentSeparator, elementSeparator } = input;
 
     const jsonObject = JSON.parse(content.toString());
-    let result = '';
 
-    for (const key in jsonObject) {
-      if (jsonObject.hasOwnProperty(key)) {
-        const values = jsonObject[key] as any[];
-        for (const obj of values) {
-          result += `${key}${elementSeparator}${Object.values(obj).join(
-            elementSeparator,
-          )}${segmentSeparator}`;
-        }
-      }
+    const segments: string[] = [];
+
+    for (const [key, values] of Object.entries(jsonObject)) {
+      this.processValues(key, values as any[], segments, elementSeparator);
     }
 
-    return result;
+    return segments.join(segmentSeparator);
+  }
+
+  private processValues(
+    key: string,
+    values: any[],
+    segments: string[],
+    elementSeparator: string,
+  ) {
+    if (Array.isArray(values)) {
+      for (const element of values) {
+        this.processObject(key, element, segments, elementSeparator);
+      }
+    }
+  }
+
+  private processObject(
+    key: string,
+    element: any,
+    segments: string[],
+    elementSeparator: string,
+  ) {
+    const elements = Object.values(element).join(elementSeparator);
+    const segment = `${key}${elementSeparator}${elements}`;
+    segments.push(segment);
   }
 }
