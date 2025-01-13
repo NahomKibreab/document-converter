7307ad7d4d0a359dd3424d0f5ed0e5e8defde4cf
NahomKibreab <nknahom@gmail.com>
2025-01-13 11:57:15 -0700

    Code refactoring for string-to-json

--- src/file-converter/strategies/string-to-json.converter.ts
+++ src/file-converter/strategies/string-to-json.converter.ts
@@ -3,33 +3,41 @@ import {
   ConversionStrategyInput,
 } from '../interfaces/conversion-strategy.interface';
 
+interface JsonResult {
+  [key: string]: Array<{ [key: string]: string }>;
+}
+
 export class StringToJsonConverter implements ConversionStrategy {
-  convert(input: ConversionStrategyInput): object {
+  convert(input: ConversionStrategyInput): JsonResult {
     const { segmentSeparator, elementSeparator, content } = input;
     const contentString = content.toString();
     const segments = contentString.split(segmentSeparator);
 
-    const result = {};
+    const jsonResult: { [key: string]: any[] } = {};
 
     segments.forEach((segment) => {
       const elements = segment.split(elementSeparator);
       const segmentName = elements[0];
-      const segmentData = {};
-
-      for (let i = 1; i < elements.length; i++) {
-        segmentData[`${segmentName}${i}`] = elements[i];
-      }
+      const segmentObject: { [key: string]: string } = {};
 
       if (!segmentName) {
         return;
       }
 
-      if (!result[segmentName]) {
-        result[segmentName] = [];
+      for (let i = 1; i < elements.length; i++) {
+        segmentObject[segmentName + i] = elements[i];
       }
-      result[segmentName].push(segmentData);
+
+      for (let i = 1; i < elements.length; i++) {
+        segmentObject[`${segmentName}${i}`] = elements[i];
+      }
+
+      if (!jsonResult[segmentName]) {
+        jsonResult[segmentName] = [];
+      }
+      jsonResult[segmentName].push(segmentObject);
     });
 
-    return result;
+    return jsonResult;
   }
 }
