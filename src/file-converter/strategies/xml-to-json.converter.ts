cb410b41a438a262addf75a5532c700b0b95d47a
NahomKibreab <nknahom@gmail.com>
2025-01-13 13:21:07 -0700

    Code refactor for xml-json

--- src/file-converter/strategies/xml-to-json.converter.ts
+++ src/file-converter/strategies/xml-to-json.converter.ts
@@ -5,36 +5,40 @@ import {
 } from '../interfaces/conversion-strategy.interface';
 
 export class XmlToJsonConverter implements ConversionStrategy {
+  private parser: XMLParser;
+
+  constructor() {
+    this.parser = new XMLParser();
+  }
+
   convert(input: ConversionStrategyInput): object {
     const { content } = input;
 
-    // Convert Buffer to string
-    const xmlString = content.toString('utf8');
+    const xmlToJsonResult = this.parser.parse(content.toString('utf8'));
 
-    // Create an instance of XMLParser
-    const parser = new XMLParser({
-      ignoreAttributes: false,
-      attributeNamePrefix: '@_',
-    });
-
-    // Parse the XML string to JSON
-    const jsonObj = parser.parse(xmlString);
-
-    // Convert all values to strings
-    const convertValuesToString = (obj: any): any => {
-      if (typeof obj === 'object' && obj !== null) {
-        for (const key in obj) {
-          if (obj.hasOwnProperty(key)) {
-            obj[key] = convertValuesToString(obj[key]);
-          }
-        }
-      } else if (typeof obj !== 'string') {
-        return String(obj);
-      }
-      return obj;
-    };
-
-    const result = convertValuesToString(jsonObj['root']);
+    const result = this.convertValuesToString(xmlToJsonResult['root']);
     return result;
+  }
+
+  private convertValuesToString(inputData: any): any {
+    if (Array.isArray(inputData)) {
+      return inputData.map((item) => this.convertValuesToString(item));
+    }
+
+    if (typeof inputData === 'object' && inputData !== null) {
+      return this.convertObjectValuesToString(inputData);
+    }
+
+    if (typeof inputData !== 'string') {
+      return inputData.toString();
+    }
+    return inputData;
+  }
+
+  private convertObjectValuesToString(obj: any): any {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+      acc[key] = this.convertValuesToString(value);
+      return acc;
+    }, {} as any);
   }
 }
